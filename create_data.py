00ccc5dcafb4ca64133372174d31ec53ad55d685
sumanthd17 <dsumanth17@gmail.com>
2020-07-29 10:12:49 +0530

    considering top 4 aspects

--- create_data.py
+++ create_data.py
@@ -4,29 +4,34 @@ train_data = parse_train_data()
 val_data = parse_val_data()
 test_data = parse_test_data()
 
+top_aspects = ["general", "price", "safety", "trans-location"]
+
 with open("QA_pairs/train-QA.csv", "w") as f:
     for td in train_data:
-        id_ = td[0]
-        ans = " ".join(td[1])
-        q = f"what do you think about the {td[3]} of {td[2]} ?"
-        sentiment = td[4]
-        f.write("\t".join([str(id_), q, ans, sentiment]))
-        f.write("\n")
+        if td[3] in top_aspects:
+            id_ = td[0]
+            ans = " ".join(td[1])
+            q = f"what do you think about the {td[3]} of {td[2]} ?"
+            sentiment = td[4]
+            f.write("\t".join([str(id_), q, ans, sentiment]))
+            f.write("\n")
 
 with open("QA_pairs/val-QA.csv", "w") as f:
-    for td in val_data:
-        id_ = td[0]
-        ans = " ".join(td[1])
-        q = f"what do you think about the {td[3]} of {td[2]} ?"
-        sentiment = td[4]
-        f.write("\t".join([str(id_), q, ans, sentiment]))
-        f.write("\n")
+    for tf in val_data:
+        if td[3] in top_aspects:
+            id_ = td[0]
+            ans = " ".join(td[1])
+            q = f"what do you think about the {td[3]} of {td[2]} ?"
+            sentiment = td[4]
+            f.write("\t".join([str(id_), q, ans, sentiment]))
+            f.write("\n")
 
 with open("QA_pairs/test-QA.csv", "w") as f:
     for td in test_data:
-        id_ = td[0]
-        ans = " ".join(td[1])
-        q = f"what do you think about the {td[3]} of {td[2]} ?"
-        sentiment = td[4]
-        f.write("\t".join([str(id_), q, ans, sentiment]))
-        f.write("\n")
+        if td[3] in top_aspects:
+            id_ = td[0]
+            ans = " ".join(td[1])
+            q = f"what do you think about the {td[3]} of {td[2]} ?"
+            sentiment = td[4]
+            f.write("\t".join([str(id_), q, ans, sentiment]))
+            f.write("\n")
